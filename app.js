8390914bf9a92b6d623fb20f7a8b69e325073a24
Jeff Luppes <jeffluppes@gmail.com>
2015-04-02 15:56:07 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,118 @@
+var MongoClient = require('mongodb').MongoClient;
+var http = require('http');
+var $ = require('cheerio');
+var process = require('process');
+var later = require('later');
+
+var result = {
+    "_id": "",
+    "type": "FeatureCollection",
+    "features": []
+};
+
+var database = "mongodb://localhost:27017/flight24";
+var collection = "planes";
+
+
+GetFlightData();
+setInterval(function () {
+  GetFlightData();
+}, 15000);
+
+function updateNameById(obj, id, value) {
+    try  {
+        if (obj == null)
+            return;
+        var done = false;
+        obj.some(function (o) {
+            if (o.properties != null && o.properties["id"] === id) {
+                o = value;
+                done = true;
+
+                //  console.log('updating feature');
+                return true;
+            } else {
+                return false;
+            }
+        });
+        if (!done) {
+            // console.log('adding feature');
+            obj.push(value);
+        }
+    } catch (e) {
+        console.log('error');
+    }
+}
+
+function CheckFeature(f) {
+    updateNameById(result.features, f["properties"]["id"], f);
+}
+
+function GetFlightData() {
+    var options = {
+        host: 'lhr.data.fr24.com',
+        path: '/zones/fcgi/feed.js?bounds=55.67283539294783,51.552131597019454,-1.5024902343745907,17.149658203125&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=900&gliders=1&stats=1&'
+    };
+
+    var r = "";
+    http.request(options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (d) {
+            r += d;
+        });
+        res.on('end', function () {
+            result.features.forEach(function (f) {
+                f.properties["Active"] = "false";
+            });
+
+            var b = JSON.parse(r);
+            for (var screen in b) {
+                var s = b[screen];
+                if (s[1] != null && s[2] != null && s[0] != null) {
+                    var sit = {
+                        "type": "Feature",
+                        "geometry": { "type": "Point", "coordinates": [s[2], s[1]] },
+                        "properties": {
+                            "id": s[0],
+                            "FeatureTypeId": "plane",
+                            "Altitude": s[4],
+                            "Track": s[3],
+                            "Speed": s[5],
+                            "Squawk": s[6],
+                            "PlaneType": s[8],
+                            "Active": "true",
+                            "Time": new Date().getTime()
+                        }
+                    };
+                    CheckFeature(sit);
+                }
+            }
+            var active = result;
+            active.features = active.features.filter(function (f) {
+                return f.properties["Active"] == "true";
+            });
+
+            console.log(result.features.length);
+            // ID to avoid having duplicate _id keys (still need to know why this happens)
+            result._id = "Flightradar_" + new Date().getTime();
+            insertIntoDB(result);
+
+        });
+    }).end();
+}
+
+// Made a function to add data to Mongo. Featurecollections are stored as docs -
+// perhaps it's better to store features as docs instead.
+function insertIntoDB(r) {
+  MongoClient.connect(database, function(err, db) {
+      if(err) throw err;
+
+      db.collection(collection).insert(r, function(err, inserted) {
+          if(err) throw err;
+          // uncomment if you need the JSON printed to your terminal.
+          // console.dir("Successfully inserted: " + JSON.stringify(inserted));
+
+          return db.close();
+      });
+  });
+}
